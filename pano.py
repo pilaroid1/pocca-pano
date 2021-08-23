08567399df906b3633fbb748cbcf22dbe7980741
Rémi Sarrailh <remi@madnerd.org>
2021-08-23 09:23:29 +0200

    Refactor for new pocca version
    - Settings are now manage with pocca.utils.app
    - While loop is change to two functions
    Run / Controls
    - Countdown is manage with pocca.display.countdown

--- pano.py
+++ pano.py
@@ -2,172 +2,130 @@
     POCCA Pano
 """
 import sys
-sys.path.append("../")
-import time
-import configparser
-import os
-import signal
-import uuid
-
+sys.path.append("/media/usb/apps")
 from pocca.display.interface import Interface
+from pocca.display.countdown import Countdown
 from pocca.vision.camera import Camera #Pi Camera Manager
-from pocca.vision.effects import Effects # OpenCV Effects
-from pocca.vision.convert import Convert # FFMPEG Conversion
 from pocca.controls.buttons import Buttons # Joystick / Buttons
 from pocca.vision.panorama import Panorama
+from pocca.utils.app import App # Application Manager (Settings / Secrets / utilities)
+import os
+import uuid
 
-import pygame
+app = App()
+app.clear_terminal()
+print(app.TEXT.LOCK_WARNING)
+print(" ~~~~~~ 📷 Pilaroid Pano 📷  ~~~~~~")
+print(" https://github.com/usini/pocca-pano")
 
+interface = Interface(app.settings, app.system)
+countdown = Countdown(app.settings, app.TEXT)
+buttons = Buttons(app.TEXT)
 
-# Detect a CTRL-C abord program interrupt, and gracefully exit the application
-def sigint_handler(signal, frame):
-    print(TEXT.DEV_STOP)
-    going = False
-    sys.exit(0)
-
-# Enable abord program interrupt
-signal.signal(signal.SIGINT, sigint_handler)
-
-settings = configparser.ConfigParser()
-settings.read("/media/usb/pocca.ini")
-if settings["APPLICATION"]["lang"] == "fr":
-    from pocca.localization.fr import TEXT
-else:
-    from pocca.localization.en import TEXT
-path_images = settings["FOLDERS"]["pictures"]
-path_temp = settings["FOLDERS"]["temp"]
-
-print("\033c", end="") # Clear Terminal
-print(" ~~~~~~ 📷 POCCA (Panoramique) 📷 ~~~~~~")
-print(TEXT.LOCK_WARNING)
-
-interface = Interface(settings)
-camera = Camera(settings, TEXT)
-effects = Effects()
-convert = Convert(TEXT)
-buttons = Buttons(TEXT)
-
-pano_images = int(settings["CAMERA"]["pano_images"])
-pano_crop = int(settings["CAMERA"]["pano_crop"])
-panorama = Panorama(path_images=path_images, path_temp=path_temp, pano_images=pano_images, image_size=(int(settings["CAMERA"]["width"]),int(settings["CAMERA"]["height"])))
-
-interface.start()
-camera.start()
+camera = Camera(app.settings, app.TEXT, app.camera_resolution)
 camera.clear_temp() # Remove Previous Images
 
-going = True
-start_timer = 0
+pano_images = int(app.settings["APPLICATION"]["pano_images"])
+pano_crop = int(app.settings["APPLICATION"]["pano_crop"])
+panorama = Panorama(path_images=app.path["images"], path_temp=app.path["temp"], pano_images=pano_images, image_size=app.camera_resolution)
 filename = ""
-countdown = 0
 
-print(" ~~~~~~~~~~~~")
+# If we exit the application, we go here
+def stop(signal, frame):
+    print(app.TEXT.SHUTDOWN_APP)
+    app.running = False
+app.stop_function(stop)
 
-while going:
+def run():
+    global countdown, filename
     # Viewfinder (Live preview)
     if interface.state == "viewfinder":
-        try:
-            # Capture frame continously
-            for frame in camera.stream.capture_continuous(camera.rawCapture, format='bgr', use_video_port=True):
-                # Get array of RGB from images
-                frame = frame.array
+        # Capture frame continously
 
-                frame_resize = camera.resize(frame, (interface.resolution))
-                interface.to_screen(frame_resize)
-                interface.top_left(interface.state)
-                interface.top_right("pano")
+        for frame in camera.stream.capture_continuous(camera.rawCapture, format='bgr', use_video_port=True):
+            # Get array of RGB from images
+            frame = frame.array
 
-                if(countdown > 0):
-                    interface.bottom(str(countdown))
+            if not app.running:
+                sys.exit(0)
 
-                if interface.state == "countdown" :
-                    if time.time() > (start_timer + 1):
-                        print(TEXT.TIMER + " : " + str(countdown))
-                        if countdown > 0:
-                            countdown = countdown - 1
-                        else:
-                            interface.state = "record"
-                        start_timer = time.time()
+            frame_resize = camera.resize(frame, (interface.resolution))
+            interface.to_screen(frame_resize)
+            interface.top_left(interface.state)
+            interface.top_right("pano")
 
-                # If we are in record mode
-                if interface.state == "record":
-                    if(camera.count() < (panorama.images - 1)):
-                        camera.save(frame, path_temp)
-                        countdown = 3
-                        start_timer = time.time()
-                        interface.state = "countdown"
-                    else:
-                        camera.save(frame, path_temp)
-                        # Try to make the timelapse
-                        filename = ""
+            if countdown.running():
+                if countdown.started:
+                    interface.bottom(str(countdown.current()))
+                else:
+                    interface.state = "record"
+
+            interface.update()
+
+            # If we are in record mode
+            if interface.state == "record":
+                if(camera.count() < (panorama.images - 1)):
+                    camera.save(frame, app.path["temp"]  + "/images")
+                    countdown.start()
+                    interface.state = "viewfinder"
+                else:
+                    camera.save(frame, app.path["temp"] + "/images")
+                    # Try to make the timelapse
+                    filename = ""
+                    interface.image("saving")
+                    try:
+                        images = os.listdir(app.path["temp"] + "/images")
+                        print(app.TEXT.JOIN_IMAGES + " img0 <--> img1")
                         interface.image("saving")
-                        try:
-                            images = ["img0.png","img1.png"]
-                            print(TEXT.JOIN_IMAGES + " img0 <--> img1")
-                            panorama.join_images(images)
-                            if panorama.status == 0 :
-                                panorama.crop_images(pano_crop)
-                                if pano_images == 4:
-                                    panorama.save(path_temp + "/pano1.png")
-                                    if panorama.status == 0 :
-                                        images = ["img2.png","img3.png"]
-                                        print(TEXT.JOIN_IMAGES + " img2 <--> img3")
-                                        panorama.join_images(images)
-                                        panorama.crop_images(pano_crop)
-                                        panorama.save(path_temp + "/pano2.png")
-                                        if panorama.status == 0:
-                                            images = ["pano1.png","pano2.png"]
-                                            print(TEXT.JOIN_IMAGES + " pano1 <--> pano2")
-                                            panorama.join_images(images)
-                                            panorama.crop_images(pano_crop)
-                            if panorama.status == 0:
-                                filename = panorama.save(path_temp + "/" "pano" + "_" + str(uuid.uuid4())[:8] + ".png")
-                                print(TEXT.PANORAMA_CONVERT_OK)
-                            else:
-                                print(TEXT.PANORAMA_CONVERT_FAILED + " " + str(panorama.status))
-                            interface.state = "preview"
-                            break
-                        except:
-                            raise
-                            print(TEXT.PANORAMA_CONVERT_FAILED + " " + str(panorama.status))
-                            interface.state = "preview"
-                            camera.clear_temp()
-                            camera.rawCapture.truncate(0)
-                            break
+                        interface.update()
+                        panorama.join_images(images)
+                        if panorama.status == 0:
+                            filename = panorama.save(app.path["images"] + "/" "pano" + "_" + str(uuid.uuid4())[:8] + ".jpg")
+                            camera.save_timestamp(filename)
+                            print(app.TEXT.PANORAMA_CONVERT_OK)
+                        else:
+                            print(app.TEXT.PANORAMA_CONVERT_FAILED + " " + str(panorama.status))
+                        interface.state = "preview"
+                    except:
+                        raise
+                        print(app.TEXT.PANORAMA_CONVERT_FAILED + " " + str(panorama.status))
+                        interface.state = "preview"
+            camera.refresh()
+            controls()
 
-                """
-                Buttons
-                Check if a button is pressed
-                """
-                # Check if a button is pressed
-                pressed = buttons.check()
+def controls():
+    """
+    Buttons
+    Check if a button is pressed
+    """
+    # Check if a button is pressed
+    pressed = buttons.check()
 
-                # If the button is pressed
-                if pressed == buttons.BTN: # or  web_action == 1:
-                    start_timer = time.time()
-                    countdown = 3
-                    interface.state = "countdown"
-                camera.rawCapture.truncate(0)
-
-        # If the video capture failed, crash gracefully
-        except Exception as error:
-            raise # Add this to check error
-            print("❌ ➡️ : " + str(error))
-            going = False
+    # If the button is pressed
+    if interface.state == "viewfinder":
+        if pressed == buttons.BTN: # or  web_action == 1:
+            if not countdown.running():
+                countdown.start()
+            interface.state = "countdown"
 
     # Preview mode (show timelapse/panorama)
     if interface.state == "preview":
-        pressed = buttons.check()
         # If in Panorama, show image
         try:
             interface.load(filename)
         except:
             interface.image("error_pano")
-        if pressed != buttons.NOACTION:
-            interface.state = "viewfinder"
-            camera.rawCapture.truncate(0)
+        interface.top_left(interface.state)
+        interface.top_right("pano")
+        interface.update()
+        while interface.state == "preview":
+            pressed = buttons.check()
+            if pressed != buttons.NOACTION:
+                interface.state = "viewfinder"
+                camera.clear_temp()
+                camera.refresh()
+            if not app.running:
+                sys.exit(0)
 
-# If we exit the application, we go here
-print(TEXT.SHUTDOWN_APP)
-camera.stop()
-interface.stop()
-sys.exit(0)
+run()
+
